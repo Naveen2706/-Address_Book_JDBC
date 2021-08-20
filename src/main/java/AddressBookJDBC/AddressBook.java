2b5138b5fc65d207ce55d086ad06eb2fc689fd4f
Naveen2706 <naveen271996@gmail.com>
2021-08-20 18:55:47 +0530

    AddressBook JDBC

---
+++ src/main/java/AddressBookJDBC/AddressBook.java
@@ -0,0 +1,64 @@
+package AddressBookJDBC;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBook {
+
+	static final Scanner s = new Scanner(System.in);
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		System.out.println(
+				" Press 1 to Reterive data\n Press 2 to Update data\n Press 3 to Reterive Data for Particular Date"
+						+ "\n Press 4 to Reterive Data for city or state");
+		int choice = s.nextInt();
+
+		switch (choice) {
+		case 1:
+			ReteriveData();
+			break;
+		case 2:
+			UpdateData();
+			break;
+		case 3:
+			ReteriveDataForParticularDate();
+			break;
+		case 4:
+			ReteriveDataForParticularCityorState();
+			break;
+		}
+	}
+
+	private static void ReteriveData() throws SQLException {
+		AddressBookRepo repo = new AddressBookRepo();
+		List<Contacts> contact = repo.findAll();
+		contact.forEach(System.out::println);
+	}
+
+	private static void UpdateData() throws SQLException {
+		System.out.println("Enter Contact_Id");
+		int Contact_ID = s.nextInt();
+
+		System.out.println("Enter Address");
+		String Address = s.next();
+
+		AddressBookRepo repo = new AddressBookRepo();
+		repo.updatedata(Contact_ID, Address);
+
+	}
+
+	private static void ReteriveDataForParticularDate() throws SQLException {
+		AddressBookRepo repo = new AddressBookRepo();
+		List<Contacts> infos = repo.findAllForParticularDate();
+		infos.forEach(System.out::println);
+
+	}
+	
+	private static void ReteriveDataForParticularCityorState() throws SQLException {
+    	AddressBookRepo repo = new AddressBookRepo();
+		List<Contacts> infos = repo.findAllForParticularCityorState();
+		infos.forEach(System.out::println);	
+	}
+}
